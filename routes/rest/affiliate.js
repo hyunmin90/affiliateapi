13ba6c8073408e284c77e291d94bb83caa31efb9
System Administrator <root@wanghyeonmin-ui-MacBook-Pro.local>
2014-10-23 11:34:56 +0900

    apibarebon

--- routes/rest/affiliate.js
+++ routes/rest/affiliate.js
@@ -15,15 +15,30 @@ router.get('/', function(req, res) {
 
 //제휴 회사 및 담당자 정보 추가
 router.get('/company',function(req,res){
-var userId= req.query.userId;	
+
+var userid= req.query.userid;	
 var contactName= req.query.contactName;
 var contactLevel= req.query.contactLevel;
 var contactEmail= req.query.contactEmail;
 var contactPhone= req.query.contactPhone;
+var companyname=req.query.companyname;
+
+
+var post=[contactName,contactEmail,contactPhone,companyname];
+
+var query = dbcon.query('INSERT INTO api_affiliates(company_contact_name,company_contact_email,company_contact_phone,company_name) VALUES(?,?,?,?)',post, function(err,rows){
+        console.log(rows);
+        console.log(err);
+        console.log(query);
+        res.json(rows);
+    });
+
+
+
 
 
 //ON SUCCESS OF SETTING COMPANY send 1
-res.json({"success": "1"});
+//res.json({"success": "1"});
 
 
 });
@@ -37,7 +52,14 @@ router.get('/:userId/feature',function(req,res){
 var userId = req.params.userId;	
 
 
-res.json({"app": app, "feature":feature, "featureState":featureState });
+	var query = dbcon.query('select service_url,service_desc,status from api_affiliates where company_contact_name= ?',userId, function(err,rows){
+        console.log(rows);
+        res.json(rows);
+    });
+
+
+
+/*res.json({"app": app, "feature":feature, "featureState":featureState });*/
 
 
 });
@@ -51,6 +73,19 @@ router.get('/affiliate/:userId/feature/:featureId',function(req,res){
 var userId = req.params.userId;	
 var featureId = req.params.featureId;
 var reason=req.query.reason;
+
+
+
+
+var post=[userId,featureId,reason];
+
+var query = dbcon.query('INSERT INTO api_affiliates(company_contact_name,company_contact_email,company_contact_phone,company_name) VALUES(?,?,?,?)',post, function(err,rows){
+        console.log(rows);
+        console.log(err);
+        console.log(query);
+        res.json(rows);
+    });
+
 
 
 
