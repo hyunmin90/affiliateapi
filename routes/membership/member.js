7ae33a9dec346f2b4306dd354cb000e2f6b149bd
System Administrator <root@Hyounminui-MacBook-Pro.local>
2014-10-28 13:58:43 +0900

    hi

--- routes/membership/member.js
+++ routes/membership/member.js
@@ -36,22 +36,6 @@ var query = dbcon.query('SELECT * FROM membership_members ORDER BY createdate DE
 
 
 
-/*
-#Member INFO
-#doamin : manage.daumtools.com
-#path : GET /member/member/calcul_point_grade
-#req : N/A
-#res : Member Information
-*/
-router.get('/calcul_point_grade',function(req,res){
-
-
-
-
-
-
-});
-
 
 /*
 #Member INFO
@@ -62,7 +46,11 @@ router.get('/calcul_point_grade',function(req,res){
 */
 router.get('/insert_from_api',function(req,res){
 
-
+var query = dbcon.query('INSERT INTO membership_members(userid,createdate,joinpath,point) SELECT DISTINCT userid,(SELECT issuedate FROM api_key AS B WHERE B.userid = api_key.userid ORDER BY issuedate LIMIT 1) AS createdate,"openapi",10 FROM api_key WHERE issuedate > ifnull((SELECT createdate FROM membership_members WHERE joinpath = "openapi" ORDER BY createdate DESC LIMIT 1),date(0000-00-00))AND userid NOT IN (SELECT userid FROM membership_members )',function(err,rows){
+	console.log(rows);
+	console.log(err);
+	res.json(rows);
+	});
 
 
 
